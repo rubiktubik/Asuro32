960cab7ce2bf4b9e04780d69a95ae622734b7a9a
Michael <rubiktubik@googlemail.com>
2009-08-19 18:57:17 +0200

    Funktionierende Initialisierung des Displays

--- main.c
+++ main.c
@@ -21,9 +21,37 @@ int main(void)
 
 /*~~~~~~~~~~~~~~~~*/
 
+/*
+ */
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util\delay.h>
+#include "include\uart.h"
+#include "include\Settings.h"
+#include "include\spi.h"
+#include "include\lcd.h"
 
 
+int main(void)
+{
+    DDRB = 0xFF;
+    PORTB = 0xFF;
+    DDRA = 0xFF;
+    PORTA =0xFF;
 
+    InitSpi();
+    LcdInit();
+    _delay_ms(1000);
+
+    disp_clear();
+    _delay_ms(1000);
+
+    while(1)
+    ;
+
+    return 0;
+}
 
 /*~~~~~~~~~~~~~~~~*/
 
